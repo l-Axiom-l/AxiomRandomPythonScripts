e267f9dcf8fb01d5b304cba793a396fb02e25c2a
Axiom <axiom@mail.gmx>
2024-09-13 23:05:59 +0200

    Changed script to be used in commandline

--- cardshufflesim.py
+++ cardshufflesim.py
@@ -1,5 +1,5 @@
 import numpy as np
-import random
+import sys
 
 def calculateDeckShuffle(maxNumber):
     test = False
@@ -34,17 +34,9 @@ def calculateDeckShuffle(maxNumber):
     #print("Final deck: ", deckMix)
     return (maxNumber, index)
 
-print("Cards : 4 > ", "Repetitions : ", calculateDeckShuffle(4)[1])
-print("Cards : 6 > ", "Repetitions : ", calculateDeckShuffle(6)[1])
-print("Cards : 8 > ", "Repetitions : ", calculateDeckShuffle(8)[1])
-print("Cards : 10 > ", "Repetitions : ", calculateDeckShuffle(10)[1])
-print("Cards : 12 > ", "Repetitions : ", calculateDeckShuffle(12)[1])
-print("Cards : 14 > ", "Repetitions : ", calculateDeckShuffle(14)[1])
-print("Cards : 16 > ", "Repetitions : ", calculateDeckShuffle(16)[1])
-print("Cards : 18 > ", "Repetitions : ", calculateDeckShuffle(18)[1])
-print("Cards : 20 > ", "Repetitions : ", calculateDeckShuffle(20)[1])
-print("Cards : 22 > ", "Repetitions : ", calculateDeckShuffle(22)[1])
-print("Cards : 24 > ", "Repetitions : ", calculateDeckShuffle(24)[1])
-print("Cards : 26 > ", "Repetitions : ", calculateDeckShuffle(26)[1])
-print("Cards : 28 > ", "Repetitions : ", calculateDeckShuffle(28)[1])
-print("Cards : 30 > ", "Repetitions : ", calculateDeckShuffle(30)[1])
+if(len(sys.argv) < 2):
+    print("Please provide the number of cards")
+    sys.exit(0)
+
+cardCount = sys.argv[1]
+print("Cards : ",cardCount, "> Repetitions : ", calculateDeckShuffle(cardCount)[1])
